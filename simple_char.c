291daf05181839ee3a2d480a94abe54156758b62
aswinas <aswin.anjaly@gmail.com>
2015-01-31 10:20:43 +0530

    Update simple_char.c

--- simple_char.c
+++ simple_char.c
@@ -28,116 +28,127 @@
 #include <linux/fcntl.h>	/* O_ACCMODE */
 #include <asm/uaccess.h>
 dev_t dev;
-int debug=1;
-int max=10;
+int debug=1;   /*    To disable prints make debug as zero        */
+int max=10;    /*    Max Data transfer in one transaction        */
 #define BUFFER_SIZE 18000
 struct cdev *mycdev;
 static int ptr=0;
 char *p,*q;
 static char device_buffer[BUFFER_SIZE];
-/////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
 int dev_open(struct inode *inode,struct file *filp)
 {
-printk("\nDevice opened\n");
-if ( (filp->f_flags & O_ACCMODE) == O_WRONLY)
-{
-p=(char *)device_buffer;
-ptr=0;
-}
-else if((filp->f_flags&O_ACCMODE)==O_RDONLY)
-q=(char *)device_buffer;
+	if(debug)
+		printk("\nDevice opened\n");
+	if ( (filp->f_flags & O_ACCMODE) == O_WRONLY)
+	{
+		p=(char *)device_buffer;
+		ptr=0;
+	}
+	else if((filp->f_flags&O_ACCMODE)==O_RDONLY)
+		q=(char *)device_buffer;
 
-return 0;
+	return 0;
 }
-///////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
 
 int dev_close(struct inode *inode,struct file *filp)
 {
-printk("\nDevice closed\n");
-return 0;
+	if(debug)
+		printk("\nDevice closed\n");
+	return 0;
 }
 
-///////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
 
 ssize_t dev_read(struct file *fp,char *buff,ssize_t length,loff_t *pos)
 {
-length=ptr;
-printk("\nread starts\n");
-printk("\nlength: %ld  *pos= %ld\n",length,*pos);
-copy_to_user(buff,q,max);
-q+=max;
-*pos+=max;
-printk("\nread ends\n");
-if(ptr-=max)
-return max;
-else 
-return 0;
+	if(debug)
+	{
+		printk("\nread starts\n");
+		printk("\nlength: %ld  *pos= %ld\n",length,*pos);
+	}
+	printk("\nret %ld\n",copy_to_user(buff,q,max));
+	q+=max;
+	*pos+=max;
+	if(debug)
+		printk("\nread ends\n");
+	if(ptr-=max)
+		return max;
+	else 
+		return 0;
 }
 
-///////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
 
 ssize_t dev_write(struct file *filp,const char *buff,size_t length,loff_t *pos)
 {
-printk("\nwrite starts\n");
-printk("\nlength: %ld  *pos= %ld\n",length,*pos);
-copy_from_user(p, buff, max);
-p+=max;
-*pos+=max;
-printk("\nwrite ends\n");
-ptr+=max;
-return max;
+	if(debug)
+	{
+		printk("\nwrite starts\n");
+		printk("\nlength: %ld  *pos= %ld\n",length,*pos);
+	}
+	printk("\nret  %ld\n",copy_from_user(p, buff, max));
+	p+=max;
+	*pos+=max;
+	if(debug)
+		printk("\nwrite ends\n");
+	ptr+=max;
+	return max;
 }
 
-///////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
 
 struct file_operations fops={
-.owner=THIS_MODULE,
-.open=dev_open,
-.read=dev_read,
-.write=dev_write,
-.release=dev_close,
+	.owner=THIS_MODULE,
+	.open=dev_open,
+	.read=dev_read,
+	.write=dev_write,
+	.release=dev_close,
 };
 
-
-///////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
 
 int dev_init(void)
 {
-int res;
-if(debug)
-	printk(KERN_INFO"\ndev_init\n");
-res=alloc_chrdev_region(&dev,0,1,"simple_char");
-if(res<0)
-	printk(KERN_ALERT"\nDevice no Allocation failed\n");
-else
+	int res;
 	if(debug)
+		printk(KERN_INFO"\ndev_init\n");
+	res=alloc_chrdev_region(&dev,0,1,"simple_char");
+	if(res<0)
+		printk(KERN_ALERT"\nDevice no Allocation failed\n");
+	else
 		printk(KERN_INFO"\nDevice no Allocation Successfull %d\n",MAJOR(dev));
-mycdev=cdev_alloc();
-mycdev->ops=&fops;
-mycdev->owner=THIS_MODULE;
-res=cdev_add(mycdev,dev,1);
-if(res<0)
-        printk(KERN_ALERT"\nAdding character device into the kernel failed\n");
-else
-        if(debug)
-        	printk(KERN_INFO"\nAdding character device into the kernel successfull\n");
+	mycdev=cdev_alloc();
+	mycdev->ops=&fops;
+	mycdev->owner=THIS_MODULE;
+	res=cdev_add(mycdev,dev,1);
+	if(res<0)
+		printk(KERN_ALERT"\nAdding character device into the kernel failed\n");
+	else
+		printk(KERN_INFO"\nAdding character device into the kernel successfull\n");
 
-return 0;
+	return 0;
 }
 
-///////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
 
 void dev_exit(void)
 {
-if(debug)
-	printk(KERN_INFO"\ndev_exit\n");
-cdev_del(mycdev);
-unregister_chrdev_region(dev,1);
-if(debug)
-	printk(KERN_DEBUG"\nfreed Device no\n");
+	if(debug)
+		printk(KERN_INFO"\ndev_exit\n");
+	cdev_del(mycdev);
+	unregister_chrdev_region(dev,1);
+	if(debug)
+		printk(KERN_DEBUG"\nfreed Device no\n");
 }
 
-///////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
 module_init(dev_init);
 module_exit(dev_exit);
-///////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
